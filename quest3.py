60d8172c3113897d630545a4b1c3254b340dcf7e
Cruz084 <cruz.torres@escolar.ifrn.edu.br>
2025-09-17 10:14:36 -0300

    Create quest3.py
    Adicionei o arquivo quest3.py ao repositório teste-cruz.

---
+++ quest3.py
@@ -0,0 +1,7 @@
+nota1 = int(input("Digite a nota do 1° bimestre:"))
+nota2 = int(input("Digite a nota do 2° bimestre:"))
+nota3 = int(input("Digite a nota do 3° bimestre:"))
+nota4 = int(input("Digite a nota do 4° bimestre:"))
+soma = nota1 + nota2 + nota3 + nota4 
+media = soma / 4
+print(media)
